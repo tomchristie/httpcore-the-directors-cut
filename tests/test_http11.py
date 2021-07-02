07bb8397d893515640cadbfe466faf0ef431ca15
Tom Christie <tom@tomchristie.com>
2021-07-02 11:41:18 +0100

    Add test for network error on HTTP/1.1

--- tests/test_http11.py
+++ tests/test_http11.py
@@ -56,6 +56,62 @@ async def test_http11_connection():
 
 
 @pytest.mark.trio
+async def test_http11_connection_unread_response():
+    """
+    If the client releases the response without reading it to termination,
+    then the connection will not be reusable.
+    """
+    origin = Origin(b"https", b"example.com", 443)
+    stream = MockNetworkStream([
+        b"HTTP/1.1 200 OK\r\n",
+        b"Content-Type: plain/text\r\n",
+        b"Content-Length: 13\r\n",
+        b"\r\n",
+        b"Hello, world!",
+    ])
+    async with HTTP11Connection(
+        origin=origin, stream=stream, keepalive_expiry=5.0
+    ) as conn:
+        url = RawURL(b"https", b"example.com", 443, b"/")
+        request = RawRequest(b"GET", url, [(b"Host", b"example.com")], ByteStream(), {})
+        async with await conn.handle_request(request) as response:
+            assert response.status == 200
+
+        assert conn.get_origin() == origin
+        assert not conn.is_idle()
+        assert conn.is_closed()
+        assert not conn.is_available()
+        assert not conn.has_expired()
+        assert repr(conn) == "<HTTP11Connection [CLOSED, Request Count: 1]>"
+
+
+@pytest.mark.trio
+async def test_http11_connection_with_network_error():
+    """
+    If a network error occurs, then no response will be returned, and the
+    connection will not be reusable.
+    """
+    origin = Origin(b"https", b"example.com", 443)
+    stream = MockNetworkStream([
+        b"Wait, this isn't valid HTTP!"
+    ])
+    async with HTTP11Connection(
+        origin=origin, stream=stream, keepalive_expiry=5.0
+    ) as conn:
+        url = RawURL(b"https", b"example.com", 443, b"/")
+        request = RawRequest(b"GET", url, [(b"Host", b"example.com")], ByteStream(), {})
+        with pytest.raises(Exception):
+            await conn.handle_request(request)
+
+        assert conn.get_origin() == origin
+        assert not conn.is_idle()
+        assert conn.is_closed()
+        assert not conn.is_available()
+        assert not conn.has_expired()
+        assert repr(conn) == "<HTTP11Connection [CLOSED, Request Count: 1]>"
+
+
+@pytest.mark.trio
 async def test_http11_connection_handles_one_active_request():
     """
     Attempting to send a request while one is already in-flight will raise
